1d3404377797d1f7f922ac5470a2f9d36636c598
VanesshaW <153980312+VanesshaW@users.noreply.github.com>
2025-07-21 13:48:09 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Dashboard Penjualan", layout="wide")
+
+st.title("📊 Aplikasi Penjualan & Stok Sederhana")
+st.write("Selamat datang! Silakan gunakan sidebar untuk navigasi.")
+
+# Tambahkan halaman, grafik, dll nanti
